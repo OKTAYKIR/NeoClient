69d1a5be8c6b6e52f543a61f055fb2c7f1ae7efd
Oktay Kır <beyazkir@hotmail.com>
2020-05-03 23:35:40 +0300

    Unused files deleted.

--- NeoClient/Extensions/StatementResultExtensions.cs
+++ NeoClient/Extensions/StatementResultExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace NeoClient.Extensions
@@ -15,6 +17,23 @@ namespace NeoClient.Extensions
             var properties = recordValue.As<INode>().Properties;
 
             return properties == null ? default : properties.Map<T>();
+        }
+
+        public static IList<object> GetValues(this IStatementResult source)
+        {
+            if (source == null)
+                return null;
+
+            var entites = new Lazy<List<object>>();
+
+            foreach (IRecord record in source)
+            {
+                var node = record.Values.As<Dictionary<string, object>>();
+
+                entites.Value.Add(node);
+            }
+
+            return entites.Value;
         }
     }
 }
